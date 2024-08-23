423e2034901ad9f5d78aa3946a9fc2471e76a3e5
FreeBies Community <111620106+indian-methods@users.noreply.github.com>
2024-08-23 05:47:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// Get modal elements
+const modal = document.getElementById("modal");
+const btn = document.querySelector(".hero-button"); // Updated to use the button in the hero section
+const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+}
